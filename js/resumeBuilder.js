2ff0d08c7534143219f0bc9639aa7c6819c2d543
pchipimo <cchipimo@gmail.com>
2015-07-16 02:02:57 -0400

    Added name and title to webpage header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// $("#main").append("Chitula Chipimo");
+
+// var awesomeThoughts = "I am Chitula and I am AWESOME!";
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+
+// console.log(awesomeThoughts);
+
+
+var formattedName = HTMLheaderName.replace("%data%", "Chitula Chipimo");
+var formattedRole = HTMLheaderRole.replace("%data%", "Android | Web Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
